acf0c6863a9b2e99a5ecf2f2a27dd598fc188f2c
adam-pietrasik <adam.pietrasik90@gmail.com>
2022-06-07 16:22:13 +0200

    Creating task and sending it back to MainActivity

--- app/src/main/java/com/example/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.todolistapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.example.todolistapp.Task.TaskActivity;
+import com.example.todolistapp.Task.TaskData;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TaskData taskData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,47 @@ public class MainActivity extends AppCompatActivity {
 
         FloatingActionButton addTaskButton = findViewById(R.id.addTask);
         addTaskButton.setOnClickListener(l -> {
-            changeActivity(TaskActivity.class);
+            changeActivity();
         });
     }
 
-    private void changeActivity(Class<?> activity){
-        Intent intent = new Intent(this, activity);
-        startActivity(intent);
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == Activity.RESULT_OK){
+                    Intent data = result.getData();
+                    if(data != null){
+                        Bundle bundle = data.getParcelableExtra("TaskInformations");
+                        taskData = bundle.getParcelable("TaskData");
+                        if(taskData != null)
+                            System.out.println(taskData);
+                    }
+
+                }
+            }
+        );
+
+    private void changeActivity(){
+        Intent intent = new Intent(this, TaskActivity.class);
+        activityResultLauncher.launch(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(taskData != null){
+            outState.putParcelable("TaskData", taskData);
+            getIntent().putExtras(outState);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        Bundle data = intent.getExtras();
+        if(data != null){
+            taskData = data.getParcelable("TaskData");
+        }
     }
 }
---
+++ app/src/main/java/com/example/todolistapp/Task/TaskData.java
@@ -0,0 +1,95 @@
+package com.example.todolistapp.Task;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TaskData implements Parcelable {
+
+    private String title;
+    private String description;
+    private String creationTime;
+    private boolean notificationEnable;
+
+    public TaskData(){}
+
+    public TaskData(String title, String description, String creationTime, boolean notificationEnable) {
+        this.title = title;
+        this.description = description;
+        this.creationTime = creationTime;
+        this.notificationEnable = notificationEnable;
+    }
+
+    protected TaskData(Parcel in) {
+        title = in.readString();
+        description = in.readString();
+        creationTime = in.readString();
+        notificationEnable = in.readByte() != 0;
+    }
+
+    public static final Creator<TaskData> CREATOR = new Creator<TaskData>() {
+        @Override
+        public TaskData createFromParcel(Parcel in) {
+            return new TaskData(in);
+        }
+
+        @Override
+        public TaskData[] newArray(int size) {
+            return new TaskData[size];
+        }
+    };
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public boolean isNotificationEnable() {
+        return notificationEnable;
+    }
+
+    public void setNotificationEnable(boolean notificationEnable) {
+        this.notificationEnable = notificationEnable;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(this.title);
+        parcel.writeString(this.description);
+        parcel.writeString(this.creationTime);
+        parcel.writeBoolean(this.notificationEnable);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskData{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", creationTime='" + creationTime + '\'' +
+                ", notificationEnable=" + notificationEnable +
+                '}';
+    }
+}
